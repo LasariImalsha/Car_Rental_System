07c45b8d079c791ce9a9461d1aa122db7bced523
Lasari Imalsha <101311493+LasariImalsha@users.noreply.github.com>
2023-02-18 20:30:44 +0530

    Added loginDto

--- BackEnd/src/main/java/lk/ijse/spring/dto/LoginDTO.java
+++ BackEnd/src/main/java/lk/ijse/spring/dto/LoginDTO.java
@@ -1,4 +1,17 @@
 package lk.ijse.spring.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class LoginDTO {
+    private String loginId;
+    private String username;
+    private String password;
+    private String role;
 }
