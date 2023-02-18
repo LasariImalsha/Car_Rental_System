cba91b70c8350cb1baa022305567692320b84240
Lasari Imalsha <101311493+LasariImalsha@users.noreply.github.com>
2023-02-18 21:06:13 +0530

    Added admin entity

--- BackEnd/src/main/java/lk/ijse/spring/entity/Admin.java
+++ BackEnd/src/main/java/lk/ijse/spring/entity/Admin.java
@@ -1,4 +1,23 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
 public class Admin {
+    private String adminId;
+    private String name;
+    private String address;
+    private String contact;
+    private String email;
+    private String username;
+    private String password;
 }
