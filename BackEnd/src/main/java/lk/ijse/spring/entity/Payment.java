72c321bcc1903cad5c7df69d432ba6363c9e0720
Lasari Imalsha <101311493+LasariImalsha@users.noreply.github.com>
2023-02-18 21:27:48 +0530

    Added payment entoty

--- BackEnd/src/main/java/lk/ijse/spring/entity/Payment.java
+++ BackEnd/src/main/java/lk/ijse/spring/entity/Payment.java
@@ -1,4 +1,28 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
 public class Payment {
+    @Id
+    private String paymentId;
+    private String date;
+    private double amount;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "rentID", referencedColumnName = "rentID",nullable = false)
+    private CarRent rental;
+
+    @ManyToOne
+    @JoinColumn(name = "customerId",referencedColumnName = "customerId",nullable = false)
+    private Customer customer;
 }
