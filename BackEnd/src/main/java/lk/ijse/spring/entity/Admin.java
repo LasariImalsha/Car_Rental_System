9b0abbe0609f50ac5543067c3f49c2a58cdeedc5
Lasari Imalsha <101311493+LasariImalsha@users.noreply.github.com>
2023-02-18 21:11:39 +0530

    Added car entity

--- BackEnd/src/main/java/lk/ijse/spring/entity/Admin.java
+++ BackEnd/src/main/java/lk/ijse/spring/entity/Admin.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,6 +14,7 @@ import javax.persistence.Entity;
 @ToString
 @Entity
 public class Admin {
+    @Id
     private String adminId;
     private String name;
     private String address;
--- BackEnd/src/main/java/lk/ijse/spring/entity/Car.java
+++ BackEnd/src/main/java/lk/ijse/spring/entity/Car.java
@@ -1,4 +1,47 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+@Entity
 public class Car {
+    @Id
+    private String registrationNO;
+    private String brand;
+    private String type;
+    private int noOfPassengers;
+    private String transmissionType;
+    private String fuelType;
+    private String color;
+    private String frontViewImg;
+    private String backViewImg;
+    private String sideViewImg;
+    private String internalViewImg;
+    private double dailyRate;
+    private double monthlyRate;
+    private double freeKmForPrice;
+    private double freeKmForDuration;
+    private double lossDamageWaiver;
+    private double priceForExtraKm;
+    private double completeKm;
+    private final String status = "Available";
+
+    @OneToMany(mappedBy = "car", cascade = CascadeType.ALL)
+    private List<CarRent> rentals = new ArrayList<>();
+
+    @OneToMany(mappedBy = "car", cascade = CascadeType.ALL)
+    private List<Maintenance> maintenances = new ArrayList<>();
 }
