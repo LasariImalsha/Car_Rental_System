750703e7d2561258b256f96c786d9c913911bb30
Lasari Imalsha <101311493+LasariImalsha@users.noreply.github.com>
2023-02-18 20:36:15 +0530

    Added carRentDto

--- BackEnd/src/main/java/lk/ijse/spring/dto/CarRentDTO.java
+++ BackEnd/src/main/java/lk/ijse/spring/dto/CarRentDTO.java
@@ -1,5 +1,21 @@
 package lk.ijse.spring.dto;
 
-public class CarRentDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class CarRentDTO {
+    private String rentId;
+    private String date;
+    private String pickUpDate;
+    private String returnDate;
+    private String status;
+    private CustomerDTO customer;
+    private CarDTO car;
+    private DriverDTO driver;
 }
