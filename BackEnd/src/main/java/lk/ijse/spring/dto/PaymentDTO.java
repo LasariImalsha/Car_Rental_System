960aeb236454f4d5339313af1e6ad6697b87484f
Lasari Imalsha <101311493+LasariImalsha@users.noreply.github.com>
2023-02-18 20:37:49 +0530

    Added paymentDto

--- BackEnd/src/main/java/lk/ijse/spring/dto/PaymentDTO.java
+++ BackEnd/src/main/java/lk/ijse/spring/dto/PaymentDTO.java
@@ -1,5 +1,20 @@
 package lk.ijse.spring.dto;
 
-public class PaymentDTO {
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class PaymentDTO {
+    private String paymentId;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private String date;
+    private double amount;
+    private CarRentDTO rental;
+    private CustomerDTO customer;
 }
