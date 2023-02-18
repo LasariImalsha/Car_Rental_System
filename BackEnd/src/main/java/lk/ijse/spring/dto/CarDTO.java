8d05edfee01c6070d25cb39c97ee8769268f16d7
Lasari Imalsha <101311493+LasariImalsha@users.noreply.github.com>
2023-02-18 20:21:52 +0530

    Added carDto

--- BackEnd/src/main/java/lk/ijse/spring/dto/CarDTO.java
+++ BackEnd/src/main/java/lk/ijse/spring/dto/CarDTO.java
@@ -1,4 +1,32 @@
 package lk.ijse.spring.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class CarDTO {
+    private String registrationNO;
+    private String brand;
+    private String type;
+    private int noOfPassengers;
+    private String transmissionType;
+    private String fuelType;
+    private String color;
+    private String frontViewImg;
+    private String backViewImg;
+    private String sideViewImg;
+    private String internalViewImg;
+    private double dailyRate;
+    private double monthlyRate;
+    private double freeKmForPrice;
+    private double freeKmForDuration;
+    private double lossDamageWaiver;
+    private double priceForExtraKm;
+    private double completeKm;
+    private final String status="Available";
 }
