79123a5d632a02ebff300c13286876ccfec11672
Lasari Imalsha <101311493+LasariImalsha@users.noreply.github.com>
2023-02-22 15:10:37 +0530

    Added webAppConfig class

--- BackEnd/src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ BackEnd/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -1,4 +1,25 @@
 package lk.ijse.spring.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages = "lk.ijse.spring")
+@EnableWebMvc
 public class WebAppConfig {
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+
+    @Bean
+    public CommonsMultipartResolver multipartResolver(){
+        CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
+        commonsMultipartResolver.setMaxUploadSize(500000);
+        return commonsMultipartResolver;
+    }
 }
