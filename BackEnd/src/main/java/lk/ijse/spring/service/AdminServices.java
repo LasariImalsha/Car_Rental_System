74019bb5b46063ebb8e6d3e4786f2eea267adde4
Lasari Imalsha <101311493+LasariImalsha@users.noreply.github.com>
2023-02-22 21:48:17 +0530

    Added adminService interface

--- BackEnd/src/main/java/lk/ijse/spring/service/AdminServices.java
+++ BackEnd/src/main/java/lk/ijse/spring/service/AdminServices.java
@@ -1,4 +1,21 @@
 package lk.ijse.spring.service;
 
+import lk.ijse.spring.dto.AdminDTO;
+
+import java.util.List;
+
 public interface AdminServices {
+    void saveAdmin(AdminDTO dto);
+
+    void updateAdmin(AdminDTO dto);
+
+    void deleteAdmin(String id);
+
+    boolean findAdminByUserName(String username);
+
+    boolean findAdminByPassword(String password);
+
+    List<AdminDTO> getAllAdmins();
+
+    String generateAdminId();
 }
