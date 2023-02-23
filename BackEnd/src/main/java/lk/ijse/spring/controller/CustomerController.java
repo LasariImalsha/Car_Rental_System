9fe79f0fbffe0f3d0d26d99d4faf5d47e09c6747
Lasari Imalsha <101311493+LasariImalsha@users.noreply.github.com>
2023-02-23 12:03:07 +0530

    Updated customer service class

--- BackEnd/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ BackEnd/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,7 +1,11 @@
 package lk.ijse.spring.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequestMapping("customer")
 public class CustomerController {
+
+
 }
