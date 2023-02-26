3b6e391477589eee633c0dee379f07e5e4dd8cc1
Lasari Imalsha <101311493+LasariImalsha@users.noreply.github.com>
2023-02-26 13:28:50 +0530

    Added payment service content

---
+++ BackEnd/src/main/java/lk/ijse/spring/controller/PaymentController.java
@@ -0,0 +1,11 @@
+package lk.ijse.spring.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("payment")
+@CrossOrigin
+public class PaymentController {
+}
