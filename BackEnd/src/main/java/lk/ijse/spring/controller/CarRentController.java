8ac796eac3f1773b0ff7727c0936dde58474b87c
Lasari Imalsha <101311493+LasariImalsha@users.noreply.github.com>
2023-02-26 08:41:44 +0530

    Updated car rent controller

--- BackEnd/src/main/java/lk/ijse/spring/controller/CarRentController.java
+++ BackEnd/src/main/java/lk/ijse/spring/controller/CarRentController.java
@@ -1,11 +1,70 @@
 package lk.ijse.spring.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.spring.dto.CarRentDTO;
+import lk.ijse.spring.service.CarRentService;
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("carRent")
 @CrossOrigin
 public class CarRentController {
+
+    @Autowired
+    CarRentService service;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil getAllCarRents() {
+        return new ResponseUtil(200, "Ok", service.getAllCarRents());
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil addCarRent(@RequestBody CarRentDTO dto) {
+        System.out.println(dto.toString());
+        service.addCarRent(dto);
+        return new ResponseUtil(200, "Saved", true);
+    }
+
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil updateCarRent(@RequestBody CarRentDTO dto) {
+        service.updateCarRent(dto);
+        return new ResponseUtil(200, "Updated", true);
+    }
+
+    @DeleteMapping(params = {"rentId"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil deleteCarRent(@RequestParam String rentId) {
+        service.deleteCarRent(rentId);
+        return new ResponseUtil(200, "Deleted", true);
+    }
+
+    @GetMapping(path = "/{rentId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil searchCarRent(@PathVariable String rentId) {
+        return new ResponseUtil(200, "Ok", service.searchCarRent(rentId));
+    }
+
+    @PutMapping(path = "/{rentId}/{status}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil updateCarRentStatus(@PathVariable String rentId, @PathVariable String status) {
+        service.updateCarRentStatus(rentId, status);
+        return new ResponseUtil(200, "Ok", true);
+    }
+
+    @GetMapping(path = "/get/{status}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil getAllCarRentsByStatus(@PathVariable String status) {
+        return new ResponseUtil(200, "Ok", service.getCarRentsByStatus(status));
+    }
+
+    @GetMapping(path = "/getCarRents/{status}/{licenceNo}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil getAllCarRentsByDrivingLicence(@PathVariable String status, @PathVariable String licenceNo) {
+        return new ResponseUtil(200, "Ok", service.getCarRentsByDrivingLicenceNo(status, licenceNo));
+    }
+
+    @GetMapping(path = "/generateRentId", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil generateRentId() {
+        return new ResponseUtil(200, "Ok", service.generateRentId());
+    }
+
 }
